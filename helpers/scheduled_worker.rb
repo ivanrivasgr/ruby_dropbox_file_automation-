9958b8e8826019d6cd22ab9d77d94908c3aceabf
Iván Rivas <117788730+ivanrivasgr@users.noreply.github.com>
2025-10-29 19:20:49 -0400

    Update scheduled_worker.rb

--- helpers/scheduled_worker.rb
+++ helpers/scheduled_worker.rb
@@ -1,1 +1,14 @@
+# frozen_string_literal: true
+
+# Worker to execute automation periodically (cron / background job runner)
+class FileAutomation::ScheduledWorker
+  def perform
+    automation = FileAutomation::Main.new(
+      dropbox_client: DropboxClient.new,          # Expected to be defined in the environment
+      data_collector_service: DataCollector.new   # Destination handler
+    )
+
+    automation.call
+  end
+end
 
